12ddd73023294481f2f817e8565ace70faa2dfba
Владимир <skyfallagain009@gmail.com>
2022-05-09 00:15:58 +0300

    переход на страницы с идентификаторами

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Topic
+from .models import Topic, Entry
 
 
 def index(request):
@@ -11,3 +11,11 @@ def topics(request):
     topics = Topic.objects.all()
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    """Выводит одну тему и все ее записи"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic':topic, "entries": entries}
+    return render(request, 'learning_logs/topic.html', context)
