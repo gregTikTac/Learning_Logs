2bd89a7874bb992bbda2666ba3d546c2ac9998ac
Владимир <skyfallagain009@gmail.com>
2022-05-08 23:55:57 +0300

    Использование шаблонирования и добавление новых функций для работы с бд

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Topic
+
 
 def index(request):
     return render(request, 'learning_logs/index.html')
 
+
+def topics(request):
+    """выводит список тем"""
+    topics = Topic.objects.all()
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
