7734801caf53285b130116e50ab727615a1d4e54
Владимир <skyfallagain009@gmail.com>
2022-05-25 20:31:08 +0300

    Создали формы тем и записей указали маршрут написали фенкцию обработки запросов post / get и создали форму, добавление тем и записей

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Topic, Entry
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 def index(request):
     return render(request, 'learning_logs/index.html')
@@ -33,3 +33,20 @@ def new_topic(request):
 
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """Добавляет новую запись"""
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
